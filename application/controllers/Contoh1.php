388b22ca0744915da58744ed87c3bec8ad683245
WP2_MuhammadZikriLazuardi <113343112+zikrilazuardi@users.noreply.github.com>
2022-09-16 21:40:49 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>" ;
+        echo"Nama saya zikri lazuardi
+             saya tinggal di daerah mempawah
+             olah raga yang saya sukai adalah
+             futsal"; 
+    }
+}
